06bc77954fe63a1bb3f2d430d7e87f229f42c4f5
Andrei Nigmatulin <andrei.nigmatulin@gmail.com>
2024-07-25 01:12:48 +0100

    added 2018/day01

---
+++ 2018/day01/run.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+def load_data(filename):
+	with open(filename, 'r') as f:
+		for line in f:
+			yield int(line)
+
+# Part One
+
+result = sum(load_data('input.txt'))
+
+print(result)
+
+# Part Two
+
+history = set([])
+
+freq = 0
+
+from itertools import cycle
+
+for delta in cycle(load_data('input.txt')):
+	freq += delta
+	if freq in history:
+		break
+	history.add(freq)
+
+print(freq)
