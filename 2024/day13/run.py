13c1939629e6990780f1693bb069e8d3879c1a5d
Andrei Nigmatulin <andrei.nigmatulin@gmail.com>
2024-12-13 17:16:16 +0000

    replaced 2024/day13 with a compact version

--- 2024/day13/run.py
+++ 2024/day13/run.py
@@ -6,57 +6,42 @@ def load_data(filename):
 		for line in f:
 			line = line.rstrip('\n')
 			if line == '':
-				yield p[0], p[1], p[2]
+				yield p
 				p = []
 			else:
 				t = line.split(': ')[1].split(', ')
-				p.append( (int(t[0][2:]), int(t[1][2:])) )
-		yield p[0], p[1], p[2]
+				p.append( np.array([int(t[0][2:]), int(t[1][2:])] ) )
+		yield p
+
+import numpy as np
 
 data = list(load_data('input.txt'))
 
 # Part One
 
-result = 0
+def solve1(a, b, prize):
+	x = np.linalg.solve([[a[0], b[0]], [a[1], b[1]]], prize)
+	if np.all(np.isclose(x, np.round(x))) and np.all(0 <= x) and np.all(x <= 100):
+		return np.dot(np.round(x).astype(int), [3, 1])
+	return 0
 
-for a, b, prize in data:
-	for n_b in range(100, -1, -1):
-		n_a = min( (prize[0] - b[0] * n_b) // a[0], (prize[1] - b[1] * n_b) // a[1] )
-		if n_a < 0 or n_a > 100:
-			continue
-		if n_a * a[0] + n_b * b[0] == prize[0] and n_a * a[1] + n_b * b[1] == prize[1]:
-			result += n_a * 3 + n_b * 1
-			break
+result = sum( solve1(a, b, prize) for a, b, prize in data )
 
 print(result)
 
 # Part Two
 
-from sympy.solvers.diophantine import diop_solve
 from sympy import symbols, solve, Eq
-n_a, n_b, t1, t2 = symbols('n_a n_b t1 t2', integer=True)
+n_a, n_b = symbols('n_a n_b', integer=True)
 
-result = 0
+def solve2(a, b, prize):
+	e1 = Eq(a[0] * n_a + b[0] * n_b, prize[0])
+	e2 = Eq(a[1] * n_a + b[1] * n_b, prize[1])
+	s = solve({e1, e2}, dict=True)
+	if s:
+		return s[0][n_a] * 3 + s[0][n_b]
+	return 0
 
-add = int(1e13)
-
-for a, b, prize in data:
-	e1 = a[0] * n_a + b[0] * n_b - prize[0] - add
-	e2 = a[1] * n_a + b[1] * n_b - prize[1] - add
-	s1 = diop_solve(e1, param=t1)
-	s2 = diop_solve(e2, param=t2)
-	if s1 == (None, None) or s2 == (None, None):
-		# No solution, for example, an odd number cannot be represented as sum of even numbers
-		continue
-	e = [Eq(n_a, s1[0]), Eq(n_b, s1[1]), Eq(n_a, s2[0]), Eq(n_b, s2[1])]
-	s = solve(e, dict=True)
-	if len(s) == 0:
-		# No solution, for example two lines are parallel
-		continue
-	n_a_value, n_b_value = s[0][n_a], s[0][n_b]
-	if n_a_value < 0 or n_b_value < 0:
-		# Not a solution by problem statement
-		continue
-	result += n_a_value * 3 + n_b_value * 1
+result = sum( solve2(a, b, prize+int(1e13)) for a, b, prize in data )
 
 print(result)
