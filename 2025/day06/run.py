a7f7bfade96a906b0dd396cbd8c8d87f2f192679
Andrei Nigmatulin <andrei.nigmatulin@gmail.com>
2025-12-07 08:59:26 +0000

    simplified 2025/day06

--- 2025/day06/run.py
+++ 2025/day06/run.py
@@ -24,23 +24,22 @@ print(sum(reduce(op, col) for op, col in zip(ops, zip(*nums))))
 # Part Two
 
 with open('input.txt') as f:
-	num_lines = map(reversed, f.read().split('\n')[:-2])
-
-ops = reversed(ops)
-op = next(ops)
-col_result = 0 if op is add else 1
+	num_lines = f.read().split('\n')[:-2]
 
 total = 0
+cols = iter(zip(*num_lines))
 
-for col in zip(*num_lines):
-	n = ''.join(col).strip()
-	if n == '':
-		total += col_result
-		op = next(ops)
-		col_result = 0 if op is add else 1
-		continue
-	col_result = op(col_result, int(n))
-
-total += col_result
+for op in ops:
+	col_result = 0 if op is add else 1
+	while True:
+		try:
+			col = next(cols)
+		except StopIteration:
+			break
+		n = ''.join(col).strip()
+		if n == '':
+			break
+		col_result = op(col_result, int(n))
+	total += col_result
 
 print(total)
